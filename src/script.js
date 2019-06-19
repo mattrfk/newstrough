1e2f61d6156d9e1d5e3f86f50c9e0f0418bbef3f
matt <mattrfk@gmail.com>
2019-06-19 14:17:33 -0700

    better keyboard navigation

--- src/script.js
+++ src/script.js
@@ -1,56 +1,121 @@
 ;'use strict';
 
-function gebi(id) { return document.getElementById(id) }
+let curpub = -1
+let curart = -1
+let pubs = undefined
+let arts = undefined
+
+let selected = undefined
 
 let tags = ""
 window.onload = function() {
 	document.addEventListener('keydown', keypress)
 
 	pubs = document.body.getElementsByClassName("source")
-
-	selected = -1
 }
 
-function addBorder(e) {
-	for(let i = 0; i < pubs.length; i++) {
-		pubs[i].style.border = "1px solid transparent"
+function select(e) {
+	function addSelectStyle(e) {
+		e.style.background = "lightgrey"
 	}
 
-	e.style.border= "1px solid black" 
+	function removeSelectStyle(e) {
+		e.style.background = null
+	}
+
+	if( selected !== undefined ){
+		removeSelectStyle(selected)
+	}
+	selected = e
+	addSelectStyle(selected)
+
+	if(!isElementVisible(e)) {
+		e.scrollIntoView()
+	}
 }
 
 function up(){
-	if( selected < 0 ){
-		return
+	function pubUp() {
+		if( curpub > 0 ) {
+			curpub--
+			if (pubs[curpub].open) {
+				open(pubs[curpub])
+				curart = arts.length - 1
+				select(arts[curart])
+			} else {
+				select(pubs[curpub])
+			}
+		}
 	}
 
-	if( selected > 0 ) {
-		selected--
-		addBorder(pubs[selected])
-	} 
+	function artUp() {
+		curart--
+		select(arts[curart])
+	}
+
+	if ( curpub >= 0 && pubs[curpub].open && curart > 0 ) {
+		artUp()
+	} else {
+		pubUp()
+	}
 
 	event.preventDefault()
 }
 
 function down() {
-	if( selected < pubs.length - 1 ) {
-		selected++
-		addBorder(pubs[selected])
+	function pubDown(){
+		if( curpub < pubs.length - 1 ) {
+			curpub++
+			if (pubs[curpub].open) {
+				open(pubs[curpub]) // this reinits the articles
+				select(arts[curart])
+			} else {
+				select(pubs[curpub])
+			}
+		}
+	}
+
+	function artDown() {
+		curart++
+		select(arts[curart])
+	}
+
+	if( curpub >= 0 && pubs[curpub].open && curart < arts.length - 1 ) {
+		artDown()
+	} else {
+		pubDown()
 	}
 
 	event.preventDefault()
 }
 
 function left() {
-	if(selected >= 0){
-		pubs[selected].open = false
+	if(curpub >= 0){
+		pubs[curpub].open = false
+		select(pubs[curpub])
+		curart = -1
+		arts = undefined
+
 		event.preventDefault()
 	}
 }
 
+function open(pub) {
+	pub.open = true
+	arts = pub.getElementsByTagName("li")
+	curart = 0
+}
+
 function right() {
-	if(selected >= 0){
-		pubs[selected].open = true
+	if(curpub >= 0) {
+		current = pubs[curpub]
+		if(current) {
+			// already open, so follow the link that is selected
+		}
+
+		open(current)
+		select(arts[curart])
+		
 		event.preventDefault()
 	}
 }
@@ -74,4 +139,25 @@ function keypress(event) {
 			right()
 			break
 	}
+}
+
+// https://stackoverflow.com/a/15203639/1487030
+function isElementVisible(el) {
+    var rect     = el.getBoundingClientRect(),
+        vWidth   = window.innerWidth || doc.documentElement.clientWidth,
+        vHeight  = window.innerHeight || doc.documentElement.clientHeight,
+        efp      = function (x, y) { return document.elementFromPoint(x, y) };     
+
+    // Return false if it's not in the viewport
+    if (rect.right < 0 || rect.bottom < 0 
+            || rect.left > vWidth || rect.top > vHeight)
+        return false;
+
+    // Return true if all of its four corners are visible
+    return (
+          el.contains(efp(rect.left,  rect.top))
+      ||  el.contains(efp(rect.right, rect.top))
+      ||  el.contains(efp(rect.right, rect.bottom))
+      ||  el.contains(efp(rect.left,  rect.bottom))
+    );
 }
